77b6faebacb7b53c0f338ca7de923935a232daf9
root <root@master-01.linux.nour.local>
2025-03-10 01:38:12 +0200

    update jenkins

--- depi-angular-app/backend/server.js
+++ depi-angular-app/backend/server.js
@@ -1,14 +1,42 @@
 const express = require('express');
 const cors = require('cors');
 const db = require('./db'); // Import the connection pool
-
 const app = express();
+
+const client = require('prom-client'); //using prom client library to write an exporter to the app
+const collectDefaultMetrics = client.collectDefaultMetrics; //prom client expose default metrics out of the box
+collectDefaultMetrics({ timeout: 5000 }); // defaut metrics will be updated every 5 sec
+
+
+const httpRequestsTotal = new client.Counter({
+  name: 'http_requests_operations_total',
+  help: 'Total number of Http requests'
+})
+
+
+const httpRequestDurationSeconds = new client.Histogram({
+  name: 'http_request_duration_seconds',
+  help: 'Duration of http request in seconds',
+  buckets: [0.1, 0.5, 2, 5, 10]
+});
+
+
 
 // Enable CORS
 app.use(cors());
 
 // API route
 app.get('/', (req, res) => {
+  // simulate sleep for a random number of milliseconds to calculate http request duration
+  var start = new Date()
+  var simulateTime = Math.floor(Math.random() * (10000 - 500 + 1) + 500)
+  // simulate the duration time
+  setTimeout(function(argument){
+    var end = new Date() - start
+    httpRequestDurationSeconds.observe(end / 1000); // convert to seconds
+}, simulateTime)
+  httpRequestsTotal.inc();
+
   // Use the pool to ping the database
   db.getConnection((err, connection) => {
     if (err) {
@@ -40,6 +68,28 @@ app.get('/', (req, res) => {
     });
   });
 });
+
+
+
+
+
+
+
+app.get('/metrics', async (req, res) => {
+  res.set('Content-Type', client.register.contentType) //set format of data
+  res.end(await client.register.metrics()) // sending the metrics to the browser
+});
+
+
+
+
+
+
+
+
+
+
+
 
 // Start server
 app.listen(3000, () => {
